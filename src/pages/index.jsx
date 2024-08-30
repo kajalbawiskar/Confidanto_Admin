d3d29b73ef8b35f007509e48f41a421df7daeb69
sbmali <137095099+sbmali@users.noreply.github.com>
2024-08-30 19:14:17 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,2 +1,2 @@
-export { default as MarketDashboard } from './marketDashboard';
-export { default as LoginPage } from './LoginPage';
+export { default as AdminSidebar } from './AdminSidebar';
+export { default as ProjectSidebar } from './ProjectSidebar';
