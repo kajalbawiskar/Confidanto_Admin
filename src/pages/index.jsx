b489c67a958d709b3e1cc534deaffac0d44eea05
sbmali <137095099+sbmali@users.noreply.github.com>
2024-09-04 19:12:37 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,3 @@
-export { default as AdminDashboard } from './AdminDashboard';
+export { default as MarketDashboard } from './marketDashboard';
 export { default as LoginPage } from './LoginPage';
-export { default as DashboardPage } from './DashboardPage';
+export { default as Waitlist } from './Waitlist';
